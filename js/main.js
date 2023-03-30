ea372c745b4d3b31a3e3722eb280715ecbba8038
Lobooooooo14 <88998991+Lobooooooo14@users.noreply.github.com>
2023-03-30 10:31:08 -0300

    adicionado redirect para as páginas

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const home = document.getElementById("home")
+const posts = document.getElementById("posts-content")
+
+
+home.addEventListener("click", () => {
+    window.location.href = window.location.origin
+})
+
+posts.childNodes.forEach((post) => {
+    post.addEventListener("click", () => {
+        let page = post.querySelector(".post-title").innerHTML
+        page = page.toLowerCase().replace(/\s+/g, '-')
+        
+        window.location.href = `${window.location.origin}/pages/${page}.html`
+    })
+})
